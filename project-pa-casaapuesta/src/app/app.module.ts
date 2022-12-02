ba728c8f778436eb0b9c75abec183677fbf7639c
daidrovo16 <daidrovo@espe.edu.ec>
2022-12-01 21:29:30 -0500

    Union

--- project-pa-casaapuesta/src/app/app.module.ts
+++ project-pa-casaapuesta/src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
 import { ComprobanteRegistroComponent } from './comprobante-registro/comprobante-registro.component';
-<<<<<<< Updated upstream
 import { PaginaprincipalComponent } from './paginaprincipal/paginaprincipal.component';
 import { MenuComponent } from './menu/menu.component';
-=======
 import { FormsModule } from '@angular/forms';
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioRegistroComponent,
     ComprobanteRegistroComponent,
-<<<<<<< Updated upstream
     PaginaprincipalComponent,
     MenuComponent
-=======
-    
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
